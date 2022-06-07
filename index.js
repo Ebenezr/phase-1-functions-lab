fe7ecd167905ea251419154a873617b838a023da
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-07 14:25:34 +0300

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(someValue){
+const distanceFromHqInBlocks = function (someValue){
 
     return someValue > 42 ?  someValue - 42 : 42 - someValue;
 }
 
-function distanceFromHqInFeet(someValue){
+const distanceFromHqInFeet = function (someValue){
     return someValue > 42 ?  (someValue - 42)*264 : (42 - someValue)*264;
 }
-function distanceTravelledInFeet(start , destination){
+const distanceTravelledInFeet =function(start , destination) {
     return destination > start? (destination- start) * 264 : (start - destination) * 264 
   
 }
-function calculatesFarePrice(start, destination){
+const calculatesFarePrice = function (start, destination) {
     
     return distanceTravelledInFeet(start, destination) < 400 ? 0
     : distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000 ?  (distanceTravelledInFeet(start, destination)-400) * 0.02
