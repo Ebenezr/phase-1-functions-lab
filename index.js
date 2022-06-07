ca3ec635ae2f84d035ba9d6bba055947b7f38b21
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-07 13:35:40 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+
+    return someValue > 42 ?  someValue - 42 : 42 - someValue;
+}
+
+function distanceFromHqInFeet(someValue){
+    return someValue > 42 ?  (someValue - 42)*264 : (42 - someValue)*264;
+}
+function distanceTravelledInFeet(start , destination){
+    let distance = destination > start? (destination- start) * 264 : (start - destination) * 264 
+    return distance;
+}
+function calculatesFarePrice(start, destination){
+    
+    return distanceTravelledInFeet(start, destination) <400? 0
+    : distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000 ?  (distanceTravelledInFeet(start, destination)-400) * 0.02
+    : distanceTravelledInFeet(start, destination) >2000 && distanceTravelledInFeet(start, destination) <= 2500 ? 25
+    : 'cannot travel that far'
+   
+
+}
+console.log(calculatesFarePrice())
